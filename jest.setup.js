c3013dc1a9e5e3d9faf63410c89bb0eaedccf87b
Doreapp <Doreapp.contact@gmail.com>
2025-12-23 17:49:13 +0100

    fix: Fix tests failing due to missing "fetch"

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const fetch = require("node-fetch");
+
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch;
+  globalThis.Headers = fetch.Headers;
+  globalThis.Request = fetch.Request;
+  globalThis.Response = fetch.Response;
+}
+
 process.env.NODE_ENV = "test";
 
 process.env.NOTION_INTEGRATION_SECRET = "test-secret";
