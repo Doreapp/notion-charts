f8ed7fbb69253e3b09cb1fe725bfd1a758225d4a
Doreapp <Doreapp.contact@gmail.com>
2025-12-23 17:42:54 +0100

    fix(test): Add missing configuration for tests

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,10 @@ const customJestConfig = {
     "^@/(.*)$": "<rootDir>/$1",
   },
   testMatch: ["**/__tests__/**/*.test.[jt]s?(x)", "**/*.test.[jt]s?(x)"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironmentOptions: {
+    customExportConditions: [""],
+  },
 };
 
 module.exports = createJestConfig(customJestConfig);
---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+process.env.NODE_ENV = "test";
+
+process.env.NOTION_INTEGRATION_SECRET = "test-secret";
+process.env.API_SECRET = "test-api-secret";
