4c2ab515b27668bc81b1f3d85fe39f4c857c16c0
Doreapp <Doreapp.contact@gmail.com>
2025-12-24 10:24:12 +0100

    chore: Enrich relation properties to enable selecting page titles instead of ids

--- app/api/databases/route.ts
+++ app/api/databases/route.ts
@@ -2,11 +2,15 @@ import { NextResponse } from "next/server";
 import { getDatabases } from "@/lib/notion/api/databases";
 import { parseDatabase } from "@/lib/parsers/database";
 import { withAuth } from "@/lib/auth/validate-secret";
+import { enrichProperties } from "@/lib/notion/properties";
 
 async function getDatabasesHandler() {
   try {
     const databases = await getDatabases();
-    const parsedDatabases = databases.map(parseDatabase);
+    const enrichedDatabases = await Promise.all(
+      databases.map(enrichProperties)
+    );
+    const parsedDatabases = enrichedDatabases.map(parseDatabase);
 
     return NextResponse.json({ databases: parsedDatabases });
   } catch (error) {
---
+++ lib/notion/properties.ts
@@ -0,0 +1,61 @@
+import { NotionDatabase } from "./api/databases";
+import { getAllDatabasePages } from "./api/database-pages";
+import { GetPageResponse, PageObjectResponse } from "@notionhq/client";
+
+export async function enrichProperties(database: NotionDatabase) {
+  for (const property of Object.values(database.properties)) {
+    if (property.type === "relation") {
+      await enrichRelationProperty(property);
+    }
+  }
+  return database;
+}
+
+type RelationProperty = Extract<
+  NotionDatabase["properties"][string],
+  { type: "relation" }
+>;
+type EnrichedRelationProperty = RelationProperty & {
+  relation: RelationProperty["relation"] & {
+    options?: {
+      id: string;
+      name?: string;
+    }[];
+  };
+};
+/** Inplace enrich a relation property with the options of the pages it points to */
+async function enrichRelationProperty(property: EnrichedRelationProperty) {
+  if (!("relation" in property)) return;
+
+  const relation = property.relation;
+  if (!relation) return;
+
+  let pages: PageObjectResponse[] = [];
+  try {
+    pages = await getAllDatabasePages(relation.data_source_id);
+  } catch (error) {
+    console.warn("Error enriching relation property", error);
+    return;
+  }
+
+  const titleProperty = findTitleProperty(pages[0]);
+  if (!titleProperty) return;
+
+  property.relation.options = pages.map((p) => {
+    const title = p.properties[titleProperty];
+    return {
+      id: p.id,
+      name:
+        title && "title" in title ? title.title?.[0]?.plain_text : undefined,
+    };
+  });
+}
+
+export function findTitleProperty(page: GetPageResponse): string | undefined {
+  if (!("properties" in page)) {
+    return undefined;
+  }
+  return Object.entries(page.properties).find(
+    ([, property]) => property.type === "title"
+  )?.[0];
+}
